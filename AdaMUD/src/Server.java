f4aae52ff6943798069a1b5f68968d9070c84914
tionajm <tionajm@grizzlies.adams.edu>
2013-10-14 11:07:23 -0600

    Initial Commit

---
+++ AdaMUD/src/Server.java
@@ -0,0 +1,15 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	private static final int PORT = 23;
+	
+	public static void main (String[] args) {
+		Server s = new Server();
+		s.listen(PORT);
+	}
+	
+	public void listen(int port) {
+		
+	}
+}
